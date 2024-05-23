2cf513c061b7ecd31c264e9baac77cad2fdb93eb
unknown <devond156@gmail.com>
2024-05-22 20:54:36 -0400

    added ui components

--- cs321-project/src/components/TransferLoader.jsx
+++ cs321-project/src/components/TransferLoader.jsx
@@ -6,7 +6,6 @@ import truck from '../assets/truck.png'
 const TransferLoader = () => {
   return (
     <div className="loader-container">
-        <div>Bar</div>
         <img className="loader-warehouse-left" src={warehouse} alt="warehouse image" />
         <img className="loader-truck" src={truck} alt="truck image" />
         <img className="loader-warehouse-right" src={warehouse} alt="warehouse image" />
--- cs321-project/src/components/TransferMenu.jsx
+++ cs321-project/src/components/TransferMenu.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import { 
+  TextInput,
+  Select,
+  SelectItem,
+  Button,
+  Form,
+  Stack
+} from 'carbon-components-react'
+
+function transfer(){
+  console.log("Do db stuff");
+  
+};
 
 const TransferMenu = () => {
   return (
-    <div>TransferMenu</div>
+    <div>
+      <Form>
+        <Stack>
+          <Select id="w-from" defaultValue="placeholder-item" labelText="From">
+            <SelectItem disabled hidden value="placeholder-item" text="Choose a warehouse" />
+            <SelectItem value="option-1" text="Warehouse 1" />
+            <SelectItem value="option-2" text="Warehouse 2" />
+            <SelectItem value="option-3" text="Warehouse 3" />
+          </Select>
+          <Select id="w-to" defaultValue="placeholder-item" labelText="To">
+            <SelectItem disabled hidden value="placeholder-item" text="Choose a warehouse" />
+            <SelectItem value="option-1" text="Warehouse 1" />
+            <SelectItem value="option-2" text="Warehouse 2" />
+            <SelectItem value="option-3" text="Warehouse 3" />
+          </Select>
+          <TextInput id="id" labelText="Product ID"/>
+          <TextInput id="q" labelText="Quantity"/>
+          <Button type="submit" className="transfer-button" onClick={transfer}>
+            Transfer
+          </Button>
+        </Stack>
+      </Form>
+    </div>
   )
 }
 
