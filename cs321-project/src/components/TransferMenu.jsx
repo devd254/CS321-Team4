c100c139f95e6fca5f9b79e66b33910a949c83bd
unknown <devond156@gmail.com>
2024-05-25 11:49:28 -0400

    ui base

--- cs321-project/src/components/TransferMenu.jsx
+++ cs321-project/src/components/TransferMenu.jsx
@@ -5,19 +5,30 @@ import {
   SelectItem,
   Button,
   Form,
-  Stack
 } from 'carbon-components-react'
 
-function transfer(){
+const transfer = (e) => {
+  e.preventDefault();
   console.log("Do db stuff");
-  
+
+  // Create a FormData object from the form element
+  const formData = new FormData(e.target);
+
+  // Retrieve the data from the TextInput fields using their IDs
+  const productId = formData.get('id');
+  const quantity = formData.get('q');
+
+  console.log(`Product ID: ${productId}`);
+  console.log(`Quantity: ${quantity}`);
+
+  //Make the truck deliver animation
+  console.log("Truck deliver");
 };
 
 const TransferMenu = () => {
   return (
-    <div>
-      <Form>
-        <Stack>
+    <div className="menu-container">
+      <Form onSubmit={transfer} >
           <Select id="w-from" defaultValue="placeholder-item" labelText="From">
             <SelectItem disabled hidden value="placeholder-item" text="Choose a warehouse" />
             <SelectItem value="option-1" text="Warehouse 1" />
@@ -30,12 +41,11 @@ const TransferMenu = () => {
             <SelectItem value="option-2" text="Warehouse 2" />
             <SelectItem value="option-3" text="Warehouse 3" />
           </Select>
-          <TextInput id="id" labelText="Product ID"/>
-          <TextInput id="q" labelText="Quantity"/>
-          <Button type="submit" className="transfer-button" onClick={transfer}>
+          <TextInput id="id" name="id" labelText="Product ID"/>
+          <TextInput id="q" name="q" labelText="Quantity"/>
+          <Button type="submit" className="transfer-button">
             Transfer
           </Button>
-        </Stack>
       </Form>
     </div>
   )
