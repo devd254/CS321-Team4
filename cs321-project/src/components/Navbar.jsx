25b044a349c03339217dad08ceb131c31e373bb6
unknown <devond156@gmail.com>
2024-05-27 13:00:16 -0400

    quick fix

--- cs321-project/src/components/Navbar.jsx
+++ cs321-project/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className="navcontainer">
-      <p className="logo">
+      <p className="nav-logo">
         SmarTrack
       </p>
       <nav className="navlinks">
