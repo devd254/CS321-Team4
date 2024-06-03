aca44cc9f43dfd167180c87524b3671d57b0a650
devd254 <devond156@gmail.com>
2024-06-03 19:29:25 -0400

    Database draft: TO FIX

--- cs321-project/src/App.jsx
+++ cs321-project/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import {useEffect, React} from "react"
 import './App.css'
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
@@ -9,10 +9,57 @@ import Add from './pages/Add'
 import Transfer from './pages/Transfer'
 import Reports from './pages/Reports'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, set } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurement Id is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDl5QW8q32mb73NIZ4bsdbvxWhTsOVsqAY",
+  authDomain: "cs321-54da7.firebaseapp.com",
+  projectId: "cs321-54da7",
+  storageBucket: "cs321-54da7.appspot.com",
+  messagingSenderId: "962075931184",
+  appId: "1:962075931184:web:2df37385a444bc7c5e73f5",
+  measurementId: "G-6DF1PBCBTN",
+  databaseURL: "https://DATABASE_NAME.REGION.firebasedatabase.app"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+async function sendData (warehouseNumber, productId, price, quantity, size, brand, type, description) {
+  try{
+    set(ref(database, 'warehouse/' + warehouseNumber), {
+      warehouseNumber: warehouseNumber,
+      productId: productId,
+      price: price,
+      quantity: quantity,
+      size: size,
+      brand: brand,
+      type: type,
+      description: description,
+    });
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
 function App() {
+  useEffect(() => {
+    sendData("1", "1", "10", "30", "small", "Gucci", "Consumer", "This bag is so cool this is a description");
+  }, []); // Empty dependency array means this runs once when the component mounts
   return (
     <Router>
       <Navbar />
+      {sendData("1", "1", "10", "30", "small", "Gucci", "Consumer", "This bag is so cool this is a description")}
         <Routes>
           <Route path="/" element={<Dashboard />}/>
           <Route path="/management" element={<Management />} />
--- cs321-project/src/components/TransferMenu.jsx
+++ cs321-project/src/components/TransferMenu.jsx
@@ -9,202 +9,31 @@ import {
 import { useState } from 'react';
 
 const TransferMenu = () => {
-  //Input values
-  const [productValue, productChange] = useState("");
-  const [quantityValue, quantityChange] = useState("");
-  const [warehouseFromValue, warehouseFromChange] = useState("");
-  const [warehouseToValue, warehouseToChange] = useState("");
-  //DB values
-  const [warehouseFromNew, warehouseFromNewChange] = useState("");
-  const [warehouseFromPrev, warehouseFromPrevChange] = useState("");
-  const [warehouseToNew, warehouseToNewChange] = useState("");
-  const [warehouseToPrev, warehouseToPrevChange] = useState("");
-
-  //Test 1
-  /*
-  * Ensures that the prev value after transferring is stored
-  * Will log to console if prev warehouse value is stored
-  * Output based on if value is stored
-  */
-  const previousFromValueStoredTest = () => {
-    if(warehouseFromPrev.localeCompare("") == 0){
-      console.log("Test 1 failed!");
-    }
-    else{
-      console.log("Test 1 passed!");
-    }
-  }
-  //Test 2
-  /*
-  * Ensures that the prev value after transferring is stored
-  * Will log to console if prev warehouse value is stored
-  * Output based on if value is stored
-  */
-  const previousToValueStoredTest = () => {
-    if(warehouseToPrev.localeCompare("") == 0){
-      console.log("Test 2 failed!");
-    }
-    else{
-      console.log("Test 2 passed!");
-    }
-  }
-  //Test 3
-  /*
-  * Ensures that the new value after transferring is stored
-  * Will log to console if new warehouse value is stored
-  * Output based on if value is stored
-  */
-  const newToValueStoredTest = () => {
-    if(warehouseToNew.localeCompare("") == 0){
-      console.log("Test 3 failed!");
-    }
-    else{
-      console.log("Test 3 passed!");
-    }
-  }
-  //Test 4
-  /*
-  * Ensures that the new value after transferring is stored
-  * Will log to console if new warehouse value is stored
-  * Output based on if value is stored
-  */
-  const newFromValueStoredTest = () => {
-    if(warehouseFromNew.localeCompare("") == 0){
-      console.log("Test 4 failed!");
-    }
-    else{
-      console.log("Test 4 passed!");
-    }
-  }
-  //Test 5
-  /*
-  * Ensures product input is properly stored
-  * Will log to console if product input stored
-  * Output based on if input was stored
-  */
-  const productInputTest = () => {
-    if(productValue.localeCompare("") == 0){
-      console.log("Test 5 failed!");
-    }
-    else{
-      console.log("Test 5 passed!");
-    }
-  }
-  //Test 6
-  /*
-  * Ensures quantity input is properly stored
-  * Will log to console if quantity input stored
-  * Output based on if input was stored
-  */
-  const quantityInputTest = () => {
-    if(quantityValue.localeCompare("") == 0){
-      console.log("Test 6 failed!");
-    }
-    else{
-      console.log("Test 6 passed!");
-    }
-  }
-  //Test 7
-  /*
-  * Ensures warehouse input is properly stored
-  * Will log to console if warehouse input stored
-  * Output based on if input was stored
-  */
-  const warehouseFromInputTest = () => {
-    if(warehouseFromValue.localeCompare("") == 0){
-      console.log("Test 7 failed!");
-    }
-    else{
-      console.log("Test 7 passed!");
-    }
-  }
-  //Test 8
-  /*
-  * Ensures warehouse input is properly stored
-  * Will log to console if warehouse input stored
-  * Output based on if input was stored
-  */
-  const warehouseToInputTest = () => {
-    if(warehouseToValue.localeCompare("") == 0){
-      console.log("Test 8 failed!");
-    }
-    else{
-      console.log("Test 8 passed!");
-    }
-  }
-  //Test 9
-  /*
-  * Ensures that the warehouse sending the transfer sent the user provided value
-  * Will log to console that the test passed
-  * Output based on difference between warehouse value after and before transfer
-  */
-  const warehouseFromTransferTest = () => {
-    //Will most likely store as int later
-    const newInt = parseInt(warehouseFromNew, 10);
-    const prevInt = parseInt(warehouseFromPrev, 10);
-    const quantityTransfered = parseInt(quantityValue, 10);
-    if(Math.abs(newInt - prevInt) != quantityTransfered){
-      console.log("Test 9 failed!");
-    }
-    else{
-      console.log("Test 9 passed!");
-    }
-  }
-  //Test 10
-  /*
-  * Ensures that the warehouse receiving the transfer received the user provided value
-  * Will log to console that the test passed
-  * Output based on difference between warehouse value after and before transfer
-  */
-  const warehouseToTransferTest = () => {
-    //Will most likely store as int later
-    const newInt = parseInt(warehouseToNew, 10);
-    const prevInt = parseInt(warehouseToPrev, 10);
-    const quantityTransfered = parseInt(quantityValue, 10);
-    if(Math.abs(newInt - prevInt) != quantityTransfered){
-      console.log("Test 10 failed!");
-    }
-    else{
-      console.log("Test 10 passed!");
-    }
-  }
+  const [dbTestNew, dbTestChanged] = useState("");
+  const [dbTestPrev, dbTestBefore] = useState("");
+  const [prodTest, prodTestChange] = useState("");
+  const [quantityTest, quantityTestChange] = useState("");
   const transfer = (e) => {
     e.preventDefault();
     console.log("Do db stuff");
-
-    //Connect to database and store previous values
-    const prevToVal = "";
-    const prevFromVal = "";
-    warehouseToPrevChange(prevToVal);
-    warehouseFromPrevChange(prevFromVal);
-
+  
     // Create a FormData object from the form element
     const formData = new FormData(e.target);
   
     // Retrieve the data from the TextInput fields using their IDs
-    const warehouseFrom = formData.get('w-from');
-    const warehouseTo = formData.get('w-to');
     const productId = formData.get('id');
     const quantity = formData.get('q');
-
     //Change state if prodId and quantity changed
-    productChange(productId);
-    quantityChange(quantity);
-    warehouseFromChange(warehouseFrom);
-    warehouseToChange(warehouseTo);
+    prodTestChange(productId);
+    quantityTestChange(quantity);
   
     console.log(`Product ID: ${productId}`);
     console.log(`Quantity: ${quantity}`);
-
-    //Subtract from From warehouse
-
-    //Add to To warehouse
     
     //Change state if db values changed
-    warehouseFromNewChange(warehouseFrom);
-    warehouseToNewChange(warehouseTo);
-
-    //Make the truck deliver animation (GUI)
+    dbTestBefore("previous database value");
+    dbTestChanged("new database value");
+    //Make the truck deliver animation
     console.log("Truck deliver");
   };
 
