33067ea15aaa6f23058ac5d8e97922d5eae829f8
Mohit Darla <118800267+mohitDarla@users.noreply.github.com>
2024-06-11 02:26:33 -0400

    Update Add.jsx

--- cs321-project/src/pages/Add.jsx
+++ cs321-project/src/pages/Add.jsx
@@ -2,6 +2,69 @@ import {NumberInput, TextArea, Select, SelectItem, Button} from '@carbon/react';
 import './../App.css';
 import React, {useState} from 'react';
 
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getDatabase, ref, set, get } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurement Id is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDl5QW8q32mb73NIZ4bsdbvxWhTsOVsqAY",
+//   authDomain: "cs321-54da7.firebaseapp.com",
+//   projectId: "cs321-54da7",
+//   storageBucket: "cs321-54da7.appspot.com",
+//   messagingSenderId: "962075931184",
+//   appId: "1:962075931184:web:2df37385a444bc7c5e73f5",
+//   measurementId: "G-6DF1PBCBTN",
+//   databaseURL: "https://DATABASE_NAME.REGION.firebasedatabase.app"
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const database = getDatabase(app);
+
+/*
+ More Documentation for database stuff here (MUST READ)
+ https://firebase.google.com/docs/database/web/read-and-write?authuser=0&hl=en#web
+*/
+// async function sendData (warehouseNumber, productId, price, quantity, size, brand, type, description) {
+//   try{
+//     console.log("Initilization and database received");
+    
+//     set(ref(database, 'warehouse/' + warehouseNumber), {
+//       warehouseNumber: warehouseNumber,
+//       productId: productId,
+//       price: price,
+//       quantity: quantity,
+//       size: size,
+//       brand: brand,
+//       type: type,
+//       description: description,
+//     });
+//     console.log("Data sent");
+//     console.log();
+//   }
+//   catch(error){
+//     console.log(error);
+//   }
+// }
+
+// async function getData (warehouseNumber) {
+//   // const dbRef = ref(database);
+//   get(ref(database, `warehouse/${warehouseNumber}`)).then((snapshot) => {
+//     if (snapshot.exists()) {
+//       console.log(snapshot.val());
+//     } else {
+//       console.log("No data available");
+//     }
+//   }).catch((error) => {
+//     console.error(error);
+//   });
+// }
 
 const Add = () => {
   const [name, setName] = useState(''); 
